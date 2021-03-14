1d1d00bf295309e6f3059c861808b192d7fc7160
Pirooz Wallace <PiroozWallace@outlook.com>
2021-03-13 19:51:01 -0800

    problems
    fg

---
+++ build/precache-manifest.0c87e0f40575df17082ebef0483f8ee5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fde130b3f77b54d5efd1560c9eb39e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "49ba45acc8359e016e8a",
+    "url": "/react-portfolio/static/css/2.54d001fc.chunk.css"
+  },
+  {
+    "revision": "c5b7467de511bdd5e18d",
+    "url": "/react-portfolio/static/css/main.6f84afec.chunk.css"
+  },
+  {
+    "revision": "49ba45acc8359e016e8a",
+    "url": "/react-portfolio/static/js/2.52ac7bd0.chunk.js"
+  },
+  {
+    "revision": "c5b7467de511bdd5e18d",
+    "url": "/react-portfolio/static/js/main.0e06d694.chunk.js"
+  },
+  {
+    "revision": "ccf743da983049655dfb",
+    "url": "/react-portfolio/static/js/runtime~main.a7f34149.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
